872a6475c812cb3659b23d84dc5e37d25d855418
Elena Rakhimova <elenara@kth.se>
2022-08-01 14:45:19 +0200

    test: use as many overrides as needed

--- src/__tests__/exercise/04-extra-3.js
+++ src/__tests__/exercise/04-extra-3.js
@@ -7,10 +7,11 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
-function buildLoginForm({username = null, password = null}) {
+function buildLoginForm(overrides) {
   return {
-    username: username || faker.internet.userName(),
-    password: password || faker.internet.password(),
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...overrides,
   }
 }
 
