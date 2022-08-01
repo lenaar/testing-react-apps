21c41f655faba482810ee9ba6f09850b112ff739
Elena Rakhimova <elenara@kth.se>
2022-08-01 13:12:39 +0200

    test: use toHaveTextContent of testing-library

---
+++ src/__tests__/exercise/02-extra.js
@@ -0,0 +1,33 @@
+// simple test with React Testing Library
+// http://localhost:3000/counter
+
+import * as React from 'react'
+// 🐨 import the `render` and `fireEvent` utilities from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
+import Counter from '../../components/counter'
+
+// NOTE: this is a new requirement in React 18
+// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#configuring-your-testing-environment
+// Luckily, it's handled for you by React Testing Library :)
+// 💣 so you can now delete this!
+// global.IS_REACT_ACT_ENVIRONMENT = true
+
+// 💣 remove this. React Testing Library does this automatically!
+// beforeEach(() => {
+//   document.body.innerHTML = ''
+// })
+
+test('counter increments and decrements when the buttons are clicked', () => {
+  const {container} = render(<Counter />)
+
+  const [decrement, increment] = container.querySelectorAll('button')
+  const message = container.firstChild.querySelector('div')
+
+  expect(message).toHaveTextContent('Current count: 0')
+
+  fireEvent.click(increment)
+  expect(message).toHaveTextContent('Current count: 1')
+
+  fireEvent.click(decrement)
+  expect(message).toHaveTextContent('Current count: 0')
+})
