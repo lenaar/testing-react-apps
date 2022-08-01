5c626ade9a85a18a68c3553a55368cc95ad8b842
Elena Rakhimova <elenara@kth.se>
2022-08-01 14:44:02 +0200

    test: use faker to generate username and password

---
+++ src/__tests__/exercise/04-extra-2.js
@@ -0,0 +1,35 @@
+// form testing
+// http://localhost:3000/login
+
+import * as React from 'react'
+import {faker} from '@faker-js/faker'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Login from '../../components/login'
+
+function buildLoginForm() {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  }
+}
+
+test('submitting the form calls onSubmit with username and password', async () => {
+  const handleSubmit = jest.fn()
+  render(<Login onSubmit={handleSubmit} />)
+
+  const userNameInput = screen.getByLabelText(/username/i)
+  const passwordInput = screen.getByLabelText(/password/i)
+  const {username, password} = buildLoginForm()
+  await userEvent.type(userNameInput, username)
+  await userEvent.type(passwordInput, password)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+  expect(handleSubmit).toHaveBeenCalledWith({password, username})
+})
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
