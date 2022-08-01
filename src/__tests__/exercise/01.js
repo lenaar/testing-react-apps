d69397572c1ccbfa7219bec728b0f13176cdc849
Elena Rakhimova <elenara@kth.se>
2022-08-01 12:32:43 +0200

    test: add test for Counter ex1

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -13,23 +13,32 @@ global.IS_REACT_ACT_ENVIRONMENT = true
 
 test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 create a div to render your component to (💰 document.createElement)
-  //
+  const container = document.createElement('div')
   // 🐨 append the div to document.body (💰 document.body.append)
-  //
+  document.body.append(container)
   // 🐨 use createRoot to render the <Counter /> to the div
+  const root = createRoot(container)
+  act(() => root.render(<Counter />))
   // 🐨 get a reference to the increment and decrement buttons:
   //   💰 div.querySelectorAll('button')
+  const [decrement, increment] = container.querySelectorAll('button')
   // 🐨 get a reference to the message div:
   //   💰 div.firstChild.querySelector('div')
-  //
+  const message = container.firstChild.querySelector('div')
   // 🐨 expect the message.textContent toBe 'Current count: 0'
+  expect(message.textContent).toBe('Current count: 0')
   // 🐨 click the increment button (💰 act(() => increment.click()))
   // 🐨 assert the message.textContent
+  act(() => increment.click())
+  expect(message.textContent).toBe('Current count: 1')
   // 🐨 click the decrement button (💰 act(() => decrement.click()))
   // 🐨 assert the message.textContent
-  //
+  act(() => decrement.click())
+  expect(message.textContent).toBe('Current count: 0')
+
   // 🐨 cleanup by removing the div from the page (💰 div.remove())
   // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+  container.remove()
 })
 
 /* eslint no-unused-vars:0 */
