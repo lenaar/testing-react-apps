4f625124ae98d60add8ea3dc63d9fd0a28fa55b3
Elena Rakhimova <elenara@kth.se>
2022-08-02 12:23:55 +0200

    test: add initial file before extra exercise 5.2

---
+++ src/__tests__/exercise/05-extra-2.js
@@ -0,0 +1,47 @@
+// mocking HTTP requests
+// http://localhost:3000/login-submission
+
+import * as React from 'react'
+// 🐨 you'll need to grab waitForElementToBeRemoved from '@testing-library/react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {build, fake} from '@jackfranklin/test-data-bot'
+import {setupServer} from 'msw/node'
+import Login from '../../components/login-submission'
+import {handlers} from '../../test/setup-handlers'
+
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
+
+const server = setupServer(...handlers)
+
+// 🐨 before all the tests, start the server with `server.listen()`
+// 🐨 after all the tests, stop the server with `server.close()`
+
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+
+test(`logging in displays the user's username`, async () => {
+  render(<Login />)
+  const {username, password} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.type(screen.getByLabelText(/password/i), password)
+  // start making the request!
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  // as soon as the user hits submit, we render a spinner to the screen. That
+  // spinner has an aria-label of "loading" for accessibility purposes, so
+  // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
+  // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  // once the login is successful, then the loading spinner disappears and
+  // we render the username.
+  // 🐨 assert that the username is on the screen
+  expect(screen.getByText(username)).toBeInTheDocument()
+})
