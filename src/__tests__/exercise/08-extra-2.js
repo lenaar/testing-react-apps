d1803e449d2ef9d52f55c4bf84658d11eb02cf6f
Elena Rakhimova <elenara@kth.se>
2022-08-03 14:34:37 +0200

    test: add initial file before extra exercise 8.2

---
+++ src/__tests__/exercise/08-extra-2.js
@@ -0,0 +1,24 @@
+// testing custom hooks
+// http://localhost:3000/counter-hook
+
+import * as React from 'react'
+import {render, act} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import useCounter from '../../components/use-counter'
+
+test('exposes the count and increment/decrement functions', () => {
+  let result
+  function TestComponent() {
+    result = useCounter()
+    return null
+  }
+  render(<TestComponent />)
+
+  expect(result.count).toBe(0)
+  act(() => result.increment())
+  expect(result.count).toBe(1)
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
+})
+
+/* eslint no-unused-vars:0 */
