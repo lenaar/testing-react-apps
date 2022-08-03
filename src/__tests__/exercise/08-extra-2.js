bb94842a4d9f44e502d335a1b85d12ad8f712143
Elena Rakhimova <elenara@kth.se>
2022-08-03 14:59:51 +0200

    test: create a custom setup of hook test

--- src/__tests__/exercise/08-extra-2.js
+++ src/__tests__/exercise/08-extra-2.js
@@ -6,14 +6,17 @@ import {render, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
-test('exposes the count and increment/decrement functions', () => {
-  let result
+function setup(props) {
+  const result = {}
   function TestComponent() {
-    result = useCounter()
+    Object.assign(result, useCounter(props))
     return null
   }
   render(<TestComponent />)
-
+  return result
+}
+test('exposes the count and increment/decrement functions', () => {
+  const result = setup()
   expect(result.count).toBe(0)
   act(() => result.increment())
   expect(result.count).toBe(1)
@@ -21,4 +24,17 @@ test('exposes the count and increment/decrement functions', () => {
   expect(result.count).toBe(0)
 })
 
+test('customize initial count', () => {
+  const result = setup({initialCount: 3})
+  expect(result.count).toBe(3)
+})
+
+test('customize step for counter', () => {
+  const result = setup({step: 4})
+  expect(result.count).toBe(0)
+  act(() => result.increment())
+  expect(result.count).toBe(4)
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
+})
 /* eslint no-unused-vars:0 */
